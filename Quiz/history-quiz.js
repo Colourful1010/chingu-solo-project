6b0c24bc1be8c8c6051ca66a989fe6e1d713e6eb
Marium <ahmedmariumm1412@gmail.com>
2025-08-15 23:08:32 +0600

    All the quiz games pages done.

---
+++ Quiz/history-quiz.js
@@ -0,0 +1,61 @@
+function checkAnswers() {
+
+    let correctAnswers = ['b', 'c', 'b', 'b', 'a', 'c', 'b', 'a', 'a', 'b'];
+    let userAnswers = [];
+
+    // Extract user answers from radio buttons
+    for (let i = 1; i <= 10; i++) {
+        let selectedAnswer = document.querySelector(`input[name="q${i}"]:checked`);
+        userAnswers.push(selectedAnswer ? selectedAnswer.value : '');
+    }
+
+    let score = 0;
+    for (let i = 0; i < correctAnswers.length; i++) {
+        if (userAnswers[i] === correctAnswers[i]) {
+            score++;
+        }
+    }
+
+    // Calculate percentage
+    let percentage = (score / correctAnswers.length) * 100;
+
+    // Determine performance analysis based on percentage
+    let performanceAnalysis;
+    if (percentage >= 80) {
+        performanceAnalysis = "Excellent";
+        } 
+    else if (percentage >= 60) {
+        performanceAnalysis = "Good";
+        } 
+    else {
+        performanceAnalysis = "Needs Improvement";
+        }
+
+    // Display results
+    document.getElementById("result").innerHTML = `
+    <center>
+        <h1>Congratulations! You have completed the quiz!</h1>
+        <h2>Here are your results.</h2>
+
+        <h3>Your name: ${document.getElementById("name").value}</h3>
+        <h3>Your total score is: ${score} out of ${correctAnswers.length}</h3>
+        <h3>Score in percentage: ${percentage.toFixed(2)}%</h3>
+        <h4>${performanceAnalysis}</h4>
+        <p>Date:${getCurrentDate()}</p>
+    </center>
+    `;
+
+    // Hide name input, questions, and submit button
+
+    document.getElementById("quiz").style.display = "none";
+    document.getElementById("submit").style.display = "none";
+    document.getElementById("result").style.display = "block";
+
+
+
+
+    function getCurrentDate() {
+    let currentDate = new Date().toLocaleDateString();
+    return currentDate;
+    }
+}
---
+++ Quiz/quiz.js
@@ -0,0 +1,6 @@
+main = document.getElementById("main");
+
+
+function history(){
+    main.innerHTML = `<h1>History Quiz</h1>`;
+}
